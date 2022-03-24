22b4f52b59da887e4f6dbdcd49eba620bca47fb5
dmpolik4rpov <91636215+dmpolik4rpov@users.noreply.github.com>
2022-03-24 18:12:13 +0300

    Update Molecule.cpp

--- Molecule.cpp
+++ Molecule.cpp
@@ -1,10 +1,4 @@
 #include "Molecule.h"
-#include <cstdio>
-#include <iostream>
-#include <fstream>
-#include <iomanip>
-#include <cassert>
-#include <cmath>
 
 
 
@@ -13,7 +7,7 @@ Molecule mol("geom.dat", 0);
 void Molecule::print_geom()
 {
     for (int i = 0; i < natom; i++)
-        printf("%d %8.5f %8.5f %8.5f\n", zvals[i], geom[i][0], geom[i][1], geom[i][2]);
+       std::printf("%d %8.5f %8.5f %8.5f\n", zvals[i], geom[i][0], geom[i][1], geom[i][2]);
 }
 
 void Molecule::translate(double x, double y, double z)
@@ -30,14 +24,13 @@ Molecule::Molecule(const char* filename, int q)
 {
     charge = q;
 
-    // Readin the file
+    // file opening
     std::ifstream is(filename);
-    assert(is.good());
 
-    // Counting the number of atoms
+    // calculation number of atoms
     is >> natom;
 
-    // New memory
+    // выделяем память
     zvals = new int[natom];
     geom = new double* [natom];
     for (int i = 0; i < natom; i++)
@@ -61,8 +54,7 @@ Molecule::~Molecule()
 double Molecule::bond(int a, int b) {
 
     double dangstr = sqrt((geom[a][0] - geom[b][0]) * (geom[a][0] - geom[b][0]) + (geom[a][1] - geom[b][1]) * (geom[a][1] - geom[b][1]) + (geom[a][2] - geom[b][2]) * (geom[a][2] - geom[b][2]));
-        dangstr = dangstr * 1.88973;///now in angstrem
-        return dangstr;
+    return dangstr;
 }
 double Molecule::unit(int cart, int a, int b)
 {
@@ -70,6 +62,5 @@ double Molecule::unit(int cart, int a, int b)
 }
 double Molecule::angle(int a, int b, int c)
 {
-    return acos(unit(0, b, a) * unit(0, b, c) + unit(1, b, a) * unit(1, b, c) + unit(2, b, a) * unit(2, b, c));
-}
-
+   double AngRad = std::acos(unit(0, b, a) * unit(0, b, c) + unit(1, b, a) * unit(1, b, c) + unit(2, b, a) * unit(2, b, c));
+   return AngRad;///here calculation comes in Radians however in Main.cpp there is a conversion into degrees
