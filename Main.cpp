cf4483f4226d7b8f4ed790ac06682d7d242ccc96
dmpolik4rpov <91636215+dmpolik4rpov@users.noreply.github.com>
2022-03-24 18:10:43 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,36 +1,30 @@
 #include "Molecule.h"
-#include <iostream>
-#include <fstream>
-#include <iomanip>
-#include <cstdio>
-#include <cmath>
 
-using namespace std;
 
 int main()
 {
-	setlocale(LC_ALL, "Russian");
 
 	Molecule mol("geom.dat", 0);
-	cout << "Number of atoms: " << mol.natom << endl;
-	cout << "Atom coordinates in Cartesian coord system:\n";
+	std::cout << "Number of atoms:" << mol.natom << std::endl;
+	std::cout << "Input Cartesian coordinates : \n";
 	mol.print_geom();
 
 
 
-	cout << "Interatomic distances in Angstrem:\n";
+	std::cout << "Interatomic distances (Bohr):\n";
 	for (int i = 0; i < mol.natom; i++)
 			for(int j = 0; j<i; j++)
-				printf("%d %d %8.5f\n", i, j, mol.bond(i, j));
+				std::printf("%d %d %8.5f\n", i, j, mol.bond(i, j));
 
 
 
-	cout << "\Bond angle:\n";
+	std::cout << "\nBond angles:\n";
+	///cout << mol.natom;
 	for (int i = 0; i < mol.natom; i++) {
 		for (int j = 0; j < i; j++) {
 			for (int k = 0; k < j; k++) {
-				if (mol.bond(i, j) < 4.0 && mol.bond(j, k) < 4.0)
-					printf("%2d-%2d-%2d %10.6f\n", i, j, k, mol.angle(i, j, k) * (180.0 / acos(-1.0)));
+				if (mol.bond(i, j) < 4 && mol.bond(j, k) < 4)
+					std::printf("%2d-%2d-%2d %10.6f\n", i, j, k, mol.angle(i, j, k) * (180.0 / std::acos(-1.0)));
 			}
 		}
 	}
