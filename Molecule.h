402faeedeaf60e31bcd2aaa13b6251a775b8f739
dmpolik4rpov <91636215+dmpolik4rpov@users.noreply.github.com>
2022-05-10 22:09:36 +0300

    Update Molecule.h

--- Molecule.h
+++ Molecule.h
@@ -17,13 +17,13 @@ public:
 	double** geom;
 	std::string point_group;
 
-	void print_geom();
-	void rotate(double phi);
+	void printGeom();
 	void translate(double x, double y, double z);
 	double bond(int atom1, int atom2);
 	double angle(int atom1, int atom2, int atom3);
-	double torsion(int atom1, int atom2, int atom3, int atom4);
+	double outOfPlane(int atom1, int atom2, int atom3, int atom4);
 	double unit(int cart, int atom1, int atom2);
+	void PrintCOM();
 
 	Molecule(const char* filename, int q);
 	~Molecule();
